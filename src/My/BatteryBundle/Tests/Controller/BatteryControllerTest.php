878ae9a164f8285b658a8836f44b22d0ca960054
dmitry.halkin <d.halkin@levi9.com>
2015-05-27 00:09:00 +0300

    some test changes

--- src/My/BatteryBundle/Tests/Controller/BatteryControllerTest.php
+++ src/My/BatteryBundle/Tests/Controller/BatteryControllerTest.php
@@ -3,64 +3,90 @@
 namespace My\BatteryBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
 
 /**
  * Class BatteryControllerTest
  *
  * @group functional
  */
-
 class BatteryControllerTest extends WebTestCase
 {
 
+    private $client;
+
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
-        $url = $client->getContainer()->get('router')->generate('battery');
-        $crawler = $client->request('GET', $url);
+        $url = $this->client->getContainer()->get('router')->generate('battery');
+        $crawler = $this->client->request('GET', $url);
 
         $this->assertTrue($crawler->filter('html:contains("Battery list")')->count() > 0);
     }
 
-    public function testSomeCases(){
 
-        $client = static::createClient();
-        $url = $client->getContainer()->get('router')->generate('battery_clear');
-        $crawler = $client->request('GET', $url);
+    public function testClear()
+    {
+
+        $url = $this->client->getContainer()->get('router')->generate('battery_clear');
+        $this->client->request('GET', $url);
+
+        $url = $this->client->getContainer()->get('router')->generate('battery');
+        $this->assertTrue($this->client->getResponse()->isRedirect($url));
+    }
 
 
-        $url = $client->getContainer()->get('router')->generate('battery_new');
-        $crawler = $client->request('GET', $url);
+    /**
+     * @dataProvider addProvider
+     * @depends      testClear
+     *
+     */
+    public function testAdd($type, $count)
+    {
+
+        $url = $this->client->getContainer()->get('router')->generate('battery_new');
+        $crawler = $this->client->request('GET', $url);
 
         $this->assertTrue($crawler->filter('html:contains("Battery creation")')->count() > 0);
 
 
         $form = $crawler->selectButton('Create')->form(array(
-            'my_batterybundle_battery[type]'  => 'AA',
-            'my_batterybundle_battery[count]'  => 4,
+            'my_batterybundle_battery[type]' => $type,
+            'my_batterybundle_battery[count]' => $count,
 
         ));
-        $client->submit($form);
+        $this->client->submit($form);
 
-        $form = $crawler->selectButton('Create')->form(array(
-            'my_batterybundle_battery[type]'  => 'AAA',
-            'my_batterybundle_battery[count]'  => 3,
+        $this->assertTrue($this->client->getResponse()->isRedirection());
+    }
 
-        ));
-        $client->submit($form);
+    /**
+     * @depends testAdd
+     *
+     */
+    public function testStat()
+    {
 
-        $form = $crawler->selectButton('Create')->form(array(
-            'my_batterybundle_battery[type]'  => 'AA',
-            'my_batterybundle_battery[count]'  => 1,
-
-        ));
-        $client->submit($form);
-
-        $url = $client->getContainer()->get('router')->generate('battery_stats');
-        $crawler = $client->request('GET', $url);
+        $url = $this->client->getContainer()->get('router')->generate('battery_stats');
+        $crawler = $this->client->request('GET', $url);
 
         $this->assertEquals(5, $crawler->filter('td[id="AA-container-count"]')->text(), 'Missing element AA');
         $this->assertEquals(3, $crawler->filter('td[id="AAA-container-count"]')->text(), 'Missing element AAA');
-
     }
+
+
+    public function addProvider()
+    {
+        return [
+            ['AA', 4],
+            ['AAA', 3],
+            ['AA', 1],
+        ];
+    }
+
 }
